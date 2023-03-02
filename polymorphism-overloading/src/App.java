29122dfa1fcb766b2608b505aa7ac33fa8ad86fb
Anderson Amantino <andersonamantino@gmail.com>
2023-03-02 15:30:16 -0300

    change dog weight

--- polymorphism-overloading/src/App.java
+++ polymorphism-overloading/src/App.java
@@ -18,9 +18,9 @@ public class App {
 
         //Reaction according to dogs age and weight
         System.out.println("if Stewie has 2 years old and 3kg:");
-        dog.reaction(2, 3);
+        dog.reaction(2, 3.7f);
 
         System.out.println("if Stewie has 5 years old and 10kg:");
-        dog.reaction(5, 10);
+        dog.reaction(5, 10.5f);
     }
 }
